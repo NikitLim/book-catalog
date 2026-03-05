7b2abe0d8d31525e2f89dec69aae4085eae622a0
NikitLim <Limonov2006@yandex.ru>
2026-03-05 19:05:58 +0300

    готовая страничка книги

--- book-details.js
+++ book-details.js
@@ -6,6 +6,7 @@ const bookTitle = document.querySelector('.name');
 const bookAuthor = document.querySelector('.author');
 const bookGenre = document.querySelector('.genre');
 const bookImage = document.querySelector('.image');
+const bookDescription = document.querySelector('.description');
 
 async function fetchBookDetails() {
     try {
@@ -20,30 +21,38 @@ async function fetchBookDetails() {
         
         // Отдельная обработка запроса автора
         try {
-            const authorResponse = await axios.get(`http://158.160.203.172:8080/author?id=${bookId}`);
-            console.log('Author details:', authorResponse.data);
-            
-            if (authorResponse.data && authorResponse.data.full_name) {
-                bookAuthor.textContent = authorResponse.data.full_name;
-            } else {
-                bookAuthor.textContent = 'Автор не найден';
-            }
+            const response = await axios.get(`http://158.160.203.172:8080/book?id=${bookId}`);
+            const book = response.data;
+            const author = book.author?.[0]; // Получаем первого автора из массива
+            bookAuthor.textContent ="Автор: " + (author ? author.full_name : 'Автор не найден');
+            console.log('Book author:', book.author);
         } catch (authorError) {
             console.warn('Ошибка при получении автора:', authorError);
             bookAuthor.textContent = 'Автор не найден';
         }
 
         try {
-            for(let i = 0; i < 10; i++) {
-                const genreResponse = await axios.get('http://158.160.203.172:8080/genre/', {
-                    params: { id: i}
-                });
-                console.log('Genre details:', genreResponse.data.books);
-
-            }
+            const response = await axios.get(`http://158.160.203.172:8080/book?id=${bookId}`);
+            const book = response.data;
+            const genres = book.genre;
+            bookGenre.textContent ="Жанр: " + (Array.isArray(genres) && genres.length
+              ? genres.map(g => g.name).join(', ')
+              : 'Жанр не найден');
+            console.log('Book genre:', book.genre);
         }
         catch (genreError) {
             console.warn('Ошибка при получении жанра:', genreError);
+        }
+
+        try {
+            const response = await axios.get(`http://158.160.203.172:8080/book?id=${bookId}`);
+            const book = response.data;
+            const description = book.description;
+            bookDescription.textContent = description ? description : 'Описание не найдено';
+            console.log('Book description:', book.description);
+        }
+        catch (descriptionError) {
+            console.warn('Ошибка при получении описания:', descriptionError);
         }
     }
     catch (error) {
